aa35610ef131d28373c7034ce41352d971f9eabf
Joakofrati <104913587+Joakofrati@users.noreply.github.com>
2025-12-04 21:49:25 -0300

    Fix: Configuración final de deploy en GitHub Pages

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,20 @@
+// app/app.config.ts
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter, withComponentInputBinding, withViewTransitions } from '@angular/router';
+import { 
+  provideRouter, 
+  withComponentInputBinding, 
+  withViewTransitions, 
+  withHashLocation // <--- 1. IMPORTAR ESTO
+} from '@angular/router';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    // withComponentInputBinding permite recibir el ID directamente como input (opcional pero util)
-    // withViewTransitions hace que el cambio de pagina sea suave
-    provideRouter(routes, withComponentInputBinding(), withViewTransitions())
+    provideRouter(
+      routes, 
+      withComponentInputBinding(), 
+      withViewTransitions(),
+      withHashLocation() // <--- 2. AGREGARLO AQUÍ
+    )
   ]
 };
--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,8 +1,11 @@
+// app.routes.server.ts
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
     path: '**',
-    renderMode: RenderMode.Prerender
+    // CAMBIO CLAVE: De Prerender a Client
+    // Esto le dice a Angular: "No intentes adivinar las rutas, deja que el navegador lo resuelva"
+    renderMode: RenderMode.Client 
   }
 ];
